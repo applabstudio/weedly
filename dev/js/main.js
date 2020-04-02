b18204b205e68fa56a854a262fc0ea215e10781b
silver <silver.designer.developer@gmail.com>
2020-04-02 16:51:40 +0200

    comment modules js

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,10 +1,10 @@
 import 'lazysizes';
-import siteHeader from './modules/site-header';
+// import siteHeader from './modules/site-header';
 import allclick from './modules/allclick';
 // import UIKit from './modules/UIKit';
-import hamburgher from './modules/hamburgher';
+// import hamburgher from './modules/hamburgher';
 
-siteHeader.init();
+// siteHeader.init();
 allclick.init();
-hamburgher.init();
+// hamburgher.init();
 // UIKit.init();
